b35fa9bab1d3aafe0d0d841a36bffccb60e9aa58
DESKTOP-643D06U\PC <phuc3851@gmail.com>
2022-01-26 14:57:28 +0700

    lesson02-data-structure 20.01.2022

---
+++ lession01-setup/src/demo/App.java
@@ -0,0 +1,11 @@
+package demo;
+
+import java.time.LocalDate;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Today is k: "+LocalDate.now());
+	}
+	
+}
---
+++ lession02-data-structure/src/structure/Ex01.java
@@ -0,0 +1,25 @@
+package structure;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("hjgygffydtd");
+		anotherMain();
+		anotherMethod();
+		anotherMain();
+
+	}
+	
+	public static void anotherMain() {
+		System.out.println("Fake Main");
+	}
+	
+	private static void anotherMethod() {
+		System.out.println("Another Method");
+	}
+	
+	private void otherMethod() {
+		System.out.println("okokokok");
+	}
+
+}
