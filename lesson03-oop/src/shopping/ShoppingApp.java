eaa38516db82836e30d7aaf354eb78fc109db8f6
Quyen Phan <Quyen.Phan@mgm-tp.com>
2022-03-06 18:03:22 +0700

    lesson03-oop 06.03.2022

--- lesson03-oop/src/shopping/ShoppingApp.java
+++ lesson03-oop/src/shopping/ShoppingApp.java
@@ -2,42 +2,35 @@ package shopping;
 
 import static shopping.DataModel.*;
 
+import java.time.LocalDate;
+
 public class ShoppingApp {
 	public static void main(String[] args) {
-		
-		// Class ==> static
-		// Object ==> non-static, static(not-prefer)
-		
 		// B1: Initial Item, Customer Data
 		Item[] items = initialItems();
 		Customer[] customers = initialCustomers();
 		
-		
 		// B2: Do shopping
 		Order[] orders = createOrders(items, customers);
 		
-		// B3: Do calculation
-		doCalculation(orders);
+		// B3: Do calculation and export bill
+		ShoppingUtils.export(orders, LocalDate.of(2022, 5, 5));
 		
-		System.out.println(orders);
-	}
-	
-	
-	private static void doCalculation(Order[] orders) {
-		for(Order order: orders) {
-			// 1 order -> n item details -> totalOfMoney
-			ItemDetail[] itemDetails = order.getItemDetails();
-			if (itemDetails.length > 0) {
-				double totalOfMoney = 0;
-				for (ItemDetail itemDetail: itemDetails) {
-					Item item = itemDetail.getItem();
-					int quantity = itemDetail.getQuantity();
-					totalOfMoney += (item.getPrice() * quantity);
+		// ===========================================
+		// Find items in "Phone" group which have price greater than 20000
+		ItemGroup[] itemGroups = initialItemGroups(items);
+		for(ItemGroup itemGroup: itemGroups) {
+			if(itemGroup.getName().equals("Phone")) {
+				Item[] phoneItems = itemGroup.getItems();
+				for (Item item: phoneItems) {
+					if (item.getPrice() > 21000) {
+						System.out.println(item);
+					}
 				}
-				order.setTotalOfMoney(totalOfMoney);
 			}
-			
 		}
 	}
+	
+	
 	
 }
