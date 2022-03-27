bcaca57192364e950d4d4212a9766f5ea3fb4195
ncdanh <nguyencongdanh0109@gmail.com>
2022-03-27 23:56:39 +0700

    lesson05-ncdanh 27.03.2022

--- lesson05-ncdanh/src/package01/IsPowerOf.java
+++ lesson05-ncdanh/src/package01/IsPowerOf.java
@@ -15,28 +15,23 @@ public class IsPowerOf {
 			System.out.println("True!");
 		}
 		if (!isPowerof) {
-			System.out.println("False");
+			System.out.println("False!");
 		}
 	}
 
 	private static boolean isPowerOf(int a, int b) {
 		int tempNumber1 = a > b ? a : b;
 		int tempNumber2 = a > b ? b : a;
-		boolean checkValue;
-		if (0 == tempNumber1 || 1 == tempNumber1) {
-			checkValue = false;
-		} else {
+		boolean checkValue = true;
 			int x = tempNumber1 / tempNumber2;
 			int y = tempNumber1 % tempNumber2;
 			while (x > 0) {
 				if (y != 0) {
 					checkValue = false;
 					break;
-				}
-				x = x / tempNumber2;
+				}		
 				y = x % tempNumber2;
-			}
-			checkValue = true;
+				x = x / tempNumber2;
 		}
 		return checkValue;
 	}
--- lesson05-ncdanh/src/package02/getMissingNumber.java
+++ lesson05-ncdanh/src/package02/getMissingNumber.java
@@ -1,10 +1,22 @@
 package package02;
 
+import java.util.Scanner;
+
 public class getMissingNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the value of n: ");
+		int n = input.nextInt();
+		int[] arr = new int[n-1];
+		
+		for(int i=1; i<=n; i++) {
+			arr[i] = i;
+			i++;
+		}
+		
+		Random rand = new random();
+		
 	}
 
 }
