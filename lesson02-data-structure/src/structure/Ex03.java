7492d8a82d466460c3dbf972d78deb58fb38aebe
manhcuong <kingmanhcuong1102000@gmail.com>
2022-04-14 09:55:04 +0700

    lesson06-exception 14.04.2022

--- lesson02-data-structure/src/structure/Ex03.java
+++ lesson02-data-structure/src/structure/Ex03.java
@@ -1,27 +1,34 @@
 package structure;
-/*format code: ctrl shift f
- * Object type 
- * +JAVA avariable object
- * +Manually created 
-*/
+
+/**
+ * Format code: ctrl shift f
+ * Object Type
+ * + JAVA available objects
+ * + Manually created objects
+ * 
+ * PRIMITIVE: variable and value store at STACK
+ * OBJECT   : variable >> STACK
+ *            value at STACK >> hashcode(address of it's value)
+ *          : value    >> HEAP
+ */
 public class Ex03 {
 	public static void main(String[] args) {
-		// Object Type : Java object types || Custom Object types
-		// Java Object type: Integer, String,Double,Float...
+		// Object Type: Java object types || Custom object types
+		// Java object types: Integer, String, Double, Float ...
 		// 2 ways to construct
-		String text = "hello"; // static type
-		String anotherText = new String("cheers");// dynamic type
+		String text = "hello";
+		String anotherText = new String("cheers");
 
 		Integer number = 10;
 		Integer anotherNumber = new Integer(100);
-		// warning in Integer contructor method
-		// @Deprecated: signature, un-encourage using (not good), able to use
+		// warning in Integer constructor method
+		// @Deprecated: signature, un-encourage using(not good), able to use
+
 		// ctrl alt down
 		System.out.println("text: " + text);
 		System.out.println("another text: " + anotherText);
 
-		// Custom Object types: Employee, Item, Oder
-
+		// Custom object types: Employee, Item, Order
 		// B1: new Item(101, 22, "IPhone X", 1000): Tạo 1 đối tượng từ Item class
 		Item itemA = new Item(101, 22, "IPhone X", 1000);
 
@@ -31,6 +38,7 @@ public class Ex03 {
 		// Định nghĩa lại hàm toString trong class Item
 		System.out.println("==============");
 		System.out.println(itemA);
+
 		// structure.Item@3d42158b
 		// return this.getClass().getName() + '@' +
 		// Integer.toHexString(this.hashCode());
@@ -38,6 +46,5 @@ public class Ex03 {
 
 		Item itemB = new Item(102, 44, "SS S10+", 800);
 		System.out.println(itemB.toString());
-
 	}
 }
